34e25100a67356f9a766184dae9a5eb2179b0be0
Steve Christian Hector OUEDRAOGO <steveouedraogo20@gmail.com>
2026-05-27 13:06:19 +0000

    Fix calculate_fess bug in Patient.py and Fix encapsulation issues by restricting direct attribute access

--- models/patient.py
+++ models/patient.py
@@ -5,37 +5,37 @@ class Patient:
     """A class representing a patient in a healthcare system."""
 
     def __init__(self, name : str, age : int) -> None:
-        self.name: str = name
-        self.age: int = age
+        self.__name: str = name
+        self.__age: int = age
         self._medical_history: dict[date, str] = {}
-        self._blood_type: str = "Unknown"
-        self.symptoms: list[str] = []
-        self.current_diagnosis: str = ""
-        self.fees_paid: float = 0.0
+        self.__blood_type: str = "Unknown"
+        self._symptoms: list[str] = []
+        self._current_diagnosis: str = ""
+        self._fees_paid: float = 0.0
 
     def add_medical_history(self, diagnosis: str) -> None:
         """Add a medical diagnosis to the patient's history."""
         self._medical_history[date.today()] = diagnosis
 
     def calculate_fees(self, amount: float) -> None:
-        """Calculate the patient's fees based on their medical history and current diagnosis."""
-        self.fees_paid += amount
+        """Add an amount to the patient's total fees paid."""
+        self._fees_paid += amount
 
     @property
     def blood_type(self) -> str:
         """Get the patient's blood type."""
-        return self._blood_type
+        return self.__blood_type
 
     @blood_type.setter
     def blood_type(self, blood_type: str) -> None:
         """Set the patient's blood type."""
         if blood_type not in ["A", "B", "AB", "O"]:
             raise ValueError("Invalid blood type. Must be one of: A, B, AB, O.")
-        self._blood_type = blood_type
+        self.__blood_type = blood_type
 
     @override
     def __str__(self) -> str:
-        return f"Patient(name={self.name}, age={self.age}, blood_type={self._blood_type}, medical_history={self._medical_history}, symptoms={self.symptoms} , current_diagnosis={self.current_diagnosis})"
+        return f"Patient(name={self.__name}, age={self.__age}, blood_type={self.__blood_type}, medical_history={self._medical_history}, symptoms={self._symptoms} , current_diagnosis={self._current_diagnosis})"
 
 
 
@@ -49,18 +49,32 @@ class UrgentPatient(Patient):
         "P4": ("Non urgent",  240),
     }
 
+    emergency_fee: float = 5000.0
+
     def __init__(self, name: str, age: int, emergency_contact: str) -> None:
         super().__init__(name, age)
-        self.emergency_contact: str = emergency_contact
-        self.triage_level: str = "P4"
+        self._triage_level: str = "P4"
+
+
+    def get_priority(self) -> str:
+        """Get the priority level of the urgent patient based on their triage level."""
+        return self.LEVELS[self._triage_level][0]
     
+
     def calculate_fees(self, amount: float) -> None:
-        self.fees_paid += amount + 5000  # Urgent patients have an additional fee of 5000
+        """Add an amount to the urgent patient's total fees paid, including the emergency fee."""
+        super().calculate_fees(amount=amount)
+
+    def get_total_fees(self) -> float:
+        """Calculate the total fees for the urgent patient, including the emergency fee."""
+        return self._fees_paid + self.emergency_fee
+
 
     @override
     def __str__(self) -> str:
-        return f"{super().__str__()}, emergency_contact={self.emergency_contact})"
-
+        return (f"{super().__str__()},"
+                f" triage_level={self._triage_level},"
+                f"priority={self.get_priority()})")
     
 
 
@@ -69,19 +83,25 @@ class HospitalizedPatient(Patient):
 
     def __init__(self, name: str, age: int, room_number: str) -> None:
         super().__init__(name, age)
-        self.room_number: str = room_number
-        self.night_passes: int = 0
+        self._room_number: str = room_number
+        self.__night_passes: int = 0
         
     def request_night_pass(self) -> None:
         """Request a night pass for the hospitalized patient."""
-        self.night_passes += 1
+        self.__night_passes += 1
 
     def calculate_fees(self, amount: float) -> None:
-        self.fees_paid += amount + (self.night_passes * 2500)  # Each night pass adds an additional fee of 2500
+        """Add an amount to the hospitalized patient's total fees paid."""
+        super().calculate_fees(amount=amount)
+
+    def get_total_fees(self) -> float:
+        """Calculate the total fees for the hospitalized patient, including any additional fees for night passes."""
+        night_pass_fee: float = 2500.0
+        return self._fees_paid + (self.__night_passes * night_pass_fee)
 
     @override
     def __str__(self) -> str:
-        return f"{super().__str__()}, room_number={self.room_number}, night_passes={self.night_passes}) "
+        return f"{super().__str__()}, room_number={self._room_number}, night_passes={self.__night_passes}) "
 
     
     
